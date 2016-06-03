e277b152faffa61d892bc71118535b086e99f8b8
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-06-03 12:56:37 +0200

    load placeholder image image on error

--- src/app/ng2-img-fallback.component.ts
+++ src/app/ng2-img-fallback.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { Ng2ImgFallback } from './ng2-img-fallback.directive'
+
 @Component({
   moduleId: module.id,
   selector: 'ng2-img-fallback-app',
   templateUrl: 'ng2-img-fallback.component.html',
-  styleUrls: ['ng2-img-fallback.component.css']
+  styleUrls: ['ng2-img-fallback.component.css'],
+  directives: [Ng2ImgFallback]
 })
 export class Ng2ImgFallbackAppComponent {
   title = 'ng2-img-fallback works!';
---
+++ src/app/ng2-img-fallback.directive.spec.ts
@@ -0,0 +1,29 @@
+import {
+  async,
+  beforeEachProviders,
+  describe,
+  ddescribe,
+  expect,
+  iit,
+  it,
+  inject
+} from '@angular/core/testing';
+import {ComponentFixture, TestComponentBuilder} from '@angular/compiler/testing';
+import { provide, Component } from '@angular/core';
+import { Ng2ImgFallback } from './ng2-img-fallback.directive';
+
+@Component({
+  selector: 'test-component',
+  template: `<div ng2-img-fallback></div>`
+})
+class TestComponent {}
+
+describe('Ng2ImgFallback Directive', () => {
+  beforeEachProviders((): any[] => []);
+
+  it('should ...', async(inject([TestComponentBuilder], (tcb:TestComponentBuilder) => {
+    return tcb.createAsync(TestComponent).then((fixture: ComponentFixture<any>) => {
+      fixture.detectChanges();
+    });
+  })));
+});
