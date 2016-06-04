33ec398575ce810d8e89e1048e6a81bf94ee2a60
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-06-04 19:05:45 +0200

    changed path

--- system.config.js
+++ system.config.js
@@ -10,7 +10,7 @@
 
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
-    'app': { main: 'main.js', defaultExtension: 'js' },
+    'app': { main: '/main.js', defaultExtension: 'js' },
     'rxjs': { defaultExtension: 'js' },
     'ng2-img-fallback': { main: 'dist/index.js' }
   };
