746ebeb91af42fffb7c2842617a362ad3fbeacc1
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-06-07 22:47:25 +0200

    enabled prod mode

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-System.register(['@angular/platform-browser-dynamic', './app'], function(exports_1, context_1) {
+System.register(['@angular/core', '@angular/platform-browser-dynamic', './app'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var platform_browser_dynamic_1, app_1;
+    var core_1, platform_browser_dynamic_1, app_1;
     return {
         setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
             function (platform_browser_dynamic_1_1) {
                 platform_browser_dynamic_1 = platform_browser_dynamic_1_1;
             },
@@ -11,6 +14,7 @@ System.register(['@angular/platform-browser-dynamic', './app'], function(exports
                 app_1 = app_1_1;
             }],
         execute: function() {
+            core_1.enableProdMode();
             platform_browser_dynamic_1.bootstrap(app_1.AppComponent, []);
         }
     }
