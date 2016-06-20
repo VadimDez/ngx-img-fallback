edf37ca7356b3ecf26ce77c4629149f5dd5a1546
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-06-20 22:41:06 +0200

    build

--- build.js
+++ build.js
@@ -4,14 +4,10 @@
 var path = require("path");
 var Builder = require('systemjs-builder');
 
-// optional constructor options
-// sets the baseURL and loads the configuration file
-var builder = new Builder('/');
-
-builder.loadConfig('./system.config.js');
+var builder = new Builder('./', './system.config.js');
 
 builder
-  .bundle('main.js', 'outfile.js')
+  .buildStatic('./app.js', './dist/build.js')
   .then(function() {
     console.log('Build complete');
   })
