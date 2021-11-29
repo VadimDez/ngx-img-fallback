7d29578872ac6fdf16452bf084fe5947227cad5c
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2021-11-29 22:23:40 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function (config) {
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
+        flags: ['--no-sandbox', '--disable-gpu']
       }
     },
     singleRun: false,
