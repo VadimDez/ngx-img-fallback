ea8ac45870dad622492841759b929944581e54d9
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2024-05-20 18:16:58 +0200

    Migrate to aangular 17

---
+++ cypress.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  videosFolder: "cypress/videos",
+  screenshotsFolder: "cypress/screenshots",
+  fixturesFolder: "cypress/fixtures",
+
+  e2e: {
+    baseUrl: 'http://localhost:4200',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
